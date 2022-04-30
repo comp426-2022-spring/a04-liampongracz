842e726bab3a4462ac76b4017f271fe904e92502
liampong <liam.pongracz@gmail.com>
2022-04-30 14:59:34 -0400

    tyring

--- database.cjs
+++ database.cjs
@@ -1,33 +1,29 @@
-const Database = require('better-sqlite3');
+// Put your database code here
+"use strict";
+const database = require("better-sqlite3")
+const db = new database("log.db")
 
-const db = new Database('log.db');
-
-const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' AND name='accesslog';
-`);
-
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
 let row = stmt.get();
 
 if (row === undefined) {
-    console.log('Database empty. Will now be initialized.');
-    const sqlInit = `
-        CREATE TABLE accesslog (id INTEGER PRIMARY KEY,
-            remoteaddr TEXT,
-            remoteuser TEXT,
-            time TIME,
-            method TEXT,
-            url TEXT,
-            protocol TEXT,
-            httpversion TEXT,
-            secure TEXT,
-            status TEXT,
-            referer TEXT,
-            useragent TEXT);
-    `;
-    db.exec(sqlInit);
-    console.log('Access log database is now initialized.');
+    console.log('Database uninitialized, now creating')
+    const sqlInit = `CREATE TABLE accesslog (
+        id INTEGER PRIMARY KEY, 
+        remoteaddr TEXT, 
+        remoteuser TEXT, 
+        time TEXT, 
+        method TEXT, 
+        url TEXT, 
+        protocol TEXT,
+        httpversion TEXT, 
+        status TEXT, 
+        referer TEXT,
+        useragent TEXT
+    );`;
+    db.exec(sqlInit)
 } else {
-    console.log('Access log exists.');
+    console.log("Database exists!");
 }
 
-module.exports = db;
+module.exports = db
--- server.js
+++ server.js
@@ -4,46 +4,54 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const express = require('express')
+const morgan = require('morgan')
+const minimist = require('minimist')
 const app = express()
-
+const fs = require('fs')
 const db = require("./database.cjs")
 
-const args = require('minimist')(process.argv.slice(2))
+const args = minimist(process.argv.slice(2))
+args["help", "port", "debug", "log"]
 
-const port = args['port'] || 5555
+const help = (`
+server.js [options]
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help	Return this message and exit.
+`)
 
-const debug = args['debug'] || false
-
-const log = args['log'] || true
-
-const fs = require('fs')
-const morgan = require('morgan')
-
-const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',port))
-});
-
-if (args['help']) {
-    console.log(`server.js [options]
-
-    --port	Set the port number for the server to listen on. Must be an integer
-                between 1 and 65535.
-  
-    --debug	If set to 'true', creates endlpoints /app/log/access/ which returns
-                a JSON access log from the database and /app/error which throws 
-                an error with the message "Error test successful." Defaults to 
-                'false'.
-  
-    --log		If set to false, no log files are written. Defaults to true.
-                Logs are always written to database.
-  
-    --help	Return this message and exit.
-    `);
-    process.exit(0);
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
 }
 
-app.use( (req, res, next) => {
-    let logdata = {
+// port default to 5555
+const port = args.port || process.env.PORT || 5000;
+const debug = (args.debug == "true");
+const log = (args.log != "false");
+
+app.use(express.json())
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }))
+
+const server = app.listen(port, () => {
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+})
+
+//Log user interaction to database
+if (log == true) {
+    const WRITESTREAM = fs.createWriteStream('access.log', { flags: 'a' });
+    app.use(morgan('combined', { stream: WRITESTREAM }));
+}
+
+app.use((req, res, next) => {
+    let logData = {
         remoteaddr: req.ip,
         remoteuser: req.user,
         time: Date.now(),
@@ -51,27 +59,12 @@ app.use( (req, res, next) => {
         url: req.url,
         protocol: req.protocol,
         httpversion: req.httpVersion,
-        secure: req.secure,
         status: res.statusCode,
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
-    };
-    const stmt = db.prepare(`
-        INSERT INTO accesslog (remoteaddr,
-        remoteuser,
-        time,
-        method,
-        url,
-        protocol,
-        httpversion,
-        secure,
-        status,
-        referer,
-        useragent) values (?,?,?,?,?,?,?,?,?,?,?);
-    `);
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time,logdata.method,
-        logdata.url,logdata.protocol,logdata.httpversion,logdata.secure,logdata.status,logdata.referer,logdata.useragent);
-    res.status(200).json(info);
+    }
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+    const info = stmt.run(logData.remoteaddr, logData.remoteuser, logData.time, logData.method, logData.url, logData.protocol, logData.httpversion, logData.status, logData.referer, logData.useragent);
     next();
 })
 
@@ -87,11 +80,6 @@ if (debug) {
     app.get('/app/error', (req, res) => {
         throw new Error('Error test successful.');
     });
-}
-
-if (log) {
-    const writestream = fs.createWriteStream('./access.log', {flags: 'a'})
-    app.use(morgan('combined',{stream:writestream}))
 }
 
 app.get('/app/', (req, res) => {
